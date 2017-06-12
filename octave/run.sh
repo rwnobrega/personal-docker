35fff741890733b03fd8dd00000ec76759515fc3
Roberto Nobrega <rwnobrega@gmail.com>
2017-06-11 21:22:14 -0300

    Minor swap of rows in run.sh

--- octave/run.sh
+++ octave/run.sh
@@ -5,11 +5,11 @@ xhost local:root > /dev/null
 docker run -ti --rm \
        -e DISPLAY=$DISPLAY \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
-       -v $HOME/.config/qt5ct/:/root/.config/qt5ct/:ro \
        -v /usr/share/fonts:/usr/share/fonts:ro \
        -v /usr/share/icons:/usr/share/icons:ro \
        -v /usr/share/themes:/usr/share/themes:ro \
-       -v $HOME/.config/octave/:/root/.config/octave/ \
+       -v $HOME/.config/qt5ct/:/root/.config/qt5ct/:ro \
        -v $HOME/.octaverc:/root/.octaverc:ro \
+       -v $HOME/.config/octave/:/root/.config/octave/ \
        -v $HOME/IFSC/Code/:$HOME/IFSC/Code/ \
        octave
